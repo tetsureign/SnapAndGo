7577dce907cd2ff284360e8a05fc43180ffb4cd5
Thu Nguyen <22362633+shortthu@users.noreply.github.com>
2022-10-12 20:04:50 +0700

    Fix maps

--- screens/MapPage.js
+++ screens/MapPage.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import MapView from 'react-native-maps';
 
-
-export default function MapPage(){
-      return(
-        <View style={styles.container} >
-            <MapView style={styles.map} />
-        </View>
-      );
+export default function MapPage() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-      container:{
-        flex :1,
-        backgroundColor : '#fff',
-        alignItems: 'center',
-        justifyContent : 'center',
-      },
-      map: {
-        width: '50%',
-        height: '50%'
-      }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
 });
