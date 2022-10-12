dd22b874d14ef739f806676de47ce3ede3bcebfc
Phu <1911185@dlu.edu.vn>
2022-10-12 19:18:40 +0700

    Them GoogleMap

--- screens/MapPage.js
+++ screens/MapPage.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { StyleSheet,Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
-const MapPage = () => {
-  return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+
+export default function MapPage(){
+  return(
+    <View style={{width:'100%', height:'100%'}}>
+                <Map style={{width:'100%', height:'75%'}}></Map>
+                
+            </View>
   );
 };
 
-export default MapPage;
+// const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       backgroundColor: '#fff',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//     },
+//     map:{
+//       width:'200',
+//       height:'200',
+//     }
+// });
