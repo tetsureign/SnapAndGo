2eb79db0f6c4295cc93de33ba215ad4f04ff8b00
Thu Nguyen <22362633+shortthu@users.noreply.github.com>
2022-10-20 14:50:05 +0700

    Fix camera stretch issue

--- screens/Camera/CameraPage.js
+++ screens/Camera/CameraPage.js
@@ -7,19 +7,25 @@ import {
   SafeAreaView,
   View,
   Button,
+  Dimensions,
+  Platform,
 } from 'react-native';
-import {Camera, CameraType} from 'expo-camera';
+import {Camera, CameraType, setHasCameraPermission} from 'expo-camera';
 import {useIsFocused} from '@react-navigation/native';
 
 const CameraPage = ({navigation}) => {
+  let camera = Camera;
+
+  // Get permission
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
 
+  // Set capture stuff
   const [previewVisible, setPreviewVisible] = useState(false);
   const [capturedImage, setCapturedImage] = useState(null);
 
+  // Check Focused
   const isFocused = useIsFocused();
-  let camera = Camera;
 
   const __takePicture = async () => {
     if (!camera) {
@@ -28,16 +34,76 @@ const CameraPage = ({navigation}) => {
     const photo = await camera.takePictureAsync();
     setPreviewVisible(true);
     setCapturedImage(photo);
+    console.log(photo);
 
     navigation.navigate('Nhận diện', photo);
+  };
+
+  // Screen Ratio and image padding
+  const [realCameraWidth, setRealCameraWidth] = useState(0);
+  const [ratio, setRatio] = useState('4:3'); // default is 4:3
+  const {height, width} = Dimensions.get('window');
+  const screenRatio = height / width;
+  const [isRatioSet, setIsRatioSet] = useState(false);
+
+  // set the camera ratio and padding.
+  // this code assumes a portrait mode screen
+  const prepareRatio = async () => {
+    let desiredRatio = '4:3'; // Start with the system default
+    // This issue only affects Android
+    if (Platform.OS === 'android') {
+      const ratios = await camera.getSupportedRatiosAsync();
+
+      // Calculate the width/height of each of the supported camera ratios
+      // These width/height are measured in landscape mode
+      // find the ratio that is closest to the screen ratio without going over
+      let distances = {};
+      let realRatios = {};
+      let minDistance = null;
+      for (const ratio of ratios) {
+        const parts = ratio.split(':');
+        const realRatio = parseInt(parts[0], 10) / parseInt(parts[1], 10);
+        realRatios[ratio] = realRatio;
+        // ratio can't be taller than screen, so we don't want an abs()
+        const distance = screenRatio - realRatio;
+        distances[ratio] = realRatio;
+        if (minDistance == null) {
+          minDistance = ratio;
+        } else {
+          if (distance >= 0 && distance < distances[minDistance]) {
+            minDistance = ratio;
+          }
+        }
+      }
+      // set the best match
+      desiredRatio = minDistance;
+      const realCameraWidth = Math.floor(
+        (width * screenRatio) / realRatios[desiredRatio],
+      );
+      setRealCameraWidth(realCameraWidth);
+      // set the preview padding and preview ratio
+      setRatio(desiredRatio);
+      // Set a flag so we don't do this
+      // calculation each time the screen refreshes
+      setIsRatioSet(true);
+    }
+  };
+
+  // the camera must be loaded in order to access the supported ratios
+  const setCameraReady = async () => {
+    if (!isRatioSet) {
+      await prepareRatio();
+    }
   };
 
   return (
     <View style={styles.container}>
       {isFocused && (
         <Camera
-          style={styles.camera}
+          onCameraReady={setCameraReady}
+          style={([styles.camera], {width: realCameraWidth, height: height})}
           type={type}
+          ratio={ratio}
           ref={r => {
             camera = r;
           }}>
@@ -78,10 +144,13 @@ const CameraPage = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
   },
   camera: {
     flex: 1,
-    width: '100%',
+    alignContent: 'center',
     zIndex: 0,
   },
   buttonContainer: {
