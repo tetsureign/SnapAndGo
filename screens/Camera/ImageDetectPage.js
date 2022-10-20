9f3ff55703c576bd2373ec654ea33d4f85832221
Thu Nguyen <22362633+shortthu@users.noreply.github.com>
2022-10-20 20:47:37 +0700

    taking data from fetch take 1

--- screens/Camera/ImageDetectPage.js
+++ screens/Camera/ImageDetectPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Text,
   StyleSheet,
@@ -7,121 +7,121 @@ import {
   View,
   ImageBackground,
 } from 'react-native';
-import {Tensor, InferenceSession} from 'onnxruntime-react-native';
-import {Asset} from 'expo-asset';
-import ndarray from 'ndarray';
-import * as FileSystem from 'expo-file-system';
-import * as tf from '@tensorflow/tfjs';
+// import {Tensor, InferenceSession} from 'onnxruntime-react-native';
+// import {Asset} from 'expo-asset';
+// import ndarray from 'ndarray';
+// import * as FileSystem from 'expo-file-system';
+// import * as tf from '@tensorflow/tfjs';
 import axios from 'axios';
 
-function preprocess(data, width, height) {
-  const dataFromImage = ndarray(new Float32Array(data), [width, height, 4]);
-  const dataProcessed = ndarray(new Float32Array(width * height * 3), [
-    1,
-    3,
-    height,
-    width,
-  ]);
+// function preprocess(data, width, height) {
+//   const dataFromImage = ndarray(new Float32Array(data), [width, height, 4]);
+//   const dataProcessed = ndarray(new Float32Array(width * height * 3), [
+//     1,
+//     3,
+//     height,
+//     width,
+//   ]);
 
-  // Normalize 0-255 to (-1)-1
-  ndarray.ops.divseq(dataFromImage, 128.0);
-  ndarray.ops.subseq(dataFromImage, 1.0);
+//   // Normalize 0-255 to (-1)-1
+//   ndarray.ops.divseq(dataFromImage, 128.0);
+//   ndarray.ops.subseq(dataFromImage, 1.0);
 
-  // Realign imageData from [224*224*4] to the correct dimension [1*3*224*224].
-  ndarray.ops.assign(
-    dataProcessed.pick(0, 0, null, null),
-    dataFromImage.pick(null, null, 2),
-  );
-  ndarray.ops.assign(
-    dataProcessed.pick(0, 1, null, null),
-    dataFromImage.pick(null, null, 1),
-  );
-  ndarray.ops.assign(
-    dataProcessed.pick(0, 2, null, null),
-    dataFromImage.pick(null, null, 0),
-  );
+//   // Realign imageData from [224*224*4] to the correct dimension [1*3*224*224].
+//   ndarray.ops.assign(
+//     dataProcessed.pick(0, 0, null, null),
+//     dataFromImage.pick(null, null, 2),
+//   );
+//   ndarray.ops.assign(
+//     dataProcessed.pick(0, 1, null, null),
+//     dataFromImage.pick(null, null, 1),
+//   );
+//   ndarray.ops.assign(
+//     dataProcessed.pick(0, 2, null, null),
+//     dataFromImage.pick(null, null, 0),
+//   );
 
-  return dataProcessed.data;
-}
+//   return dataProcessed.data;
+// }
 
-const convertPhotoBase64 = async photo => {
-  const imgUri = photo.uri;
-  const imgB64 = await FileSystem.readAsStringAsync(imgUri, {
-    encoding: FileSystem.EncodingType.Base64,
-  });
-  return imgB64;
-};
+// const convertPhotoBase64 = async photo => {
+//   const imgUri = photo.uri;
+//   const imgB64 = await FileSystem.readAsStringAsync(imgUri, {
+//     encoding: FileSystem.EncodingType.Base64,
+//   });
+//   return imgB64;
+// };
 
-const TensorConvert = async photo => {
-  const imgUri = photo.uri;
-  const imgB64 = await FileSystem.readAsStringAsync(imgUri, {
-    encoding: FileSystem.EncodingType.Base64,
-  });
-  const imgBuffer = tf.util.encodeString(imgB64, 'base64').buffer;
-  const raw = new Float32Array(imgBuffer);
-  const imageTensor = new Tensor(raw, [1, 640, 480, 3]);
-  console.log(imageTensor);
-  return imageTensor;
-  // const raw = new Uint8Array(imgBuffer);
-  // const imageTensor = new Tensor(raw, [1, 2]);
-  // console.log(imageTensor);
-  // return imageTensor;
+// const TensorConvert = async photo => {
+//   const imgUri = photo.uri;
+//   const imgB64 = await FileSystem.readAsStringAsync(imgUri, {
+//     encoding: FileSystem.EncodingType.Base64,
+//   });
+//   const imgBuffer = tf.util.encodeString(imgB64, 'base64').buffer;
+//   const raw = new Float32Array(imgBuffer);
+//   const imageTensor = new Tensor(raw, [1, 640, 480, 3]);
+//   console.log(imageTensor);
+//   return imageTensor;
+//   // const raw = new Uint8Array(imgBuffer);
+//   // const imageTensor = new Tensor(raw, [1, 2]);
+//   // console.log(imageTensor);
+//   // return imageTensor;
 
-  // const imageData = photo.getImageData(
-  //   0,
-  //   0,
-  //   photo.canvas.width,
-  //   photo.canvas.height,
-  // );
-  // const {data, width, height} = imageData;
-  // const dataTensor = ndarray(new Float32Array(data), [width, height, 4]);
-  // const dataProcessedTensor = ndarray(new Float32Array(width * height * 3), [
-  //   1,
-  //   3,
-  //   width,
-  //   height,
-  // ]);
-  // ops.assign(
-  //   dataProcessedTensor.pick(0, 0, null, null),
-  //   dataTensor.pick(null, null, 2),
-  // );
-  // ops.assign(
-  //   dataProcessedTensor.pick(0, 1, null, null),
-  //   dataTensor.pick(null, null, 1),
-  // );
-  // ops.assign(
-  //   dataProcessedTensor.pick(0, 2, null, null),
-  //   dataTensor.pick(null, null, 0),
-  // );
-  // ops.divseq(dataProcessedTensor, 255);
-  // ops.subseq(dataProcessedTensor.pick(0, 0, null, null), 0.485);
-  // ops.subseq(dataProcessedTensor.pick(0, 1, null, null), 0.456);
-  // ops.subseq(dataProcessedTensor.pick(0, 2, null, null), 0.406);
-  // ops.divseq(dataProcessedTensor.pick(0, 0, null, null), 0.229);
-  // ops.divseq(dataProcessedTensor.pick(0, 1, null, null), 0.224);
-  // ops.divseq(dataProcessedTensor.pick(0, 2, null, null), 0.225);
-  // const tensor = new Tensor(new Float32Array(3 * width * height), 'float32', [
-  //   1,
-  //   3,
-  //   width,
-  //   height,
-  // ]);
-  // tensor.data.set(dataProcessedTensor.data);
-};
+//   // const imageData = photo.getImageData(
+//   //   0,
+//   //   0,
+//   //   photo.canvas.width,
+//   //   photo.canvas.height,
+//   // );
+//   // const {data, width, height} = imageData;
+//   // const dataTensor = ndarray(new Float32Array(data), [width, height, 4]);
+//   // const dataProcessedTensor = ndarray(new Float32Array(width * height * 3), [
+//   //   1,
+//   //   3,
+//   //   width,
+//   //   height,
+//   // ]);
+//   // ops.assign(
+//   //   dataProcessedTensor.pick(0, 0, null, null),
+//   //   dataTensor.pick(null, null, 2),
+//   // );
+//   // ops.assign(
+//   //   dataProcessedTensor.pick(0, 1, null, null),
+//   //   dataTensor.pick(null, null, 1),
+//   // );
+//   // ops.assign(
+//   //   dataProcessedTensor.pick(0, 2, null, null),
+//   //   dataTensor.pick(null, null, 0),
+//   // );
+//   // ops.divseq(dataProcessedTensor, 255);
+//   // ops.subseq(dataProcessedTensor.pick(0, 0, null, null), 0.485);
+//   // ops.subseq(dataProcessedTensor.pick(0, 1, null, null), 0.456);
+//   // ops.subseq(dataProcessedTensor.pick(0, 2, null, null), 0.406);
+//   // ops.divseq(dataProcessedTensor.pick(0, 0, null, null), 0.229);
+//   // ops.divseq(dataProcessedTensor.pick(0, 1, null, null), 0.224);
+//   // ops.divseq(dataProcessedTensor.pick(0, 2, null, null), 0.225);
+//   // const tensor = new Tensor(new Float32Array(3 * width * height), 'float32', [
+//   //   1,
+//   //   3,
+//   //   width,
+//   //   height,
+//   // ]);
+//   // tensor.data.set(dataProcessedTensor.data);
+// };
 
-async function Detect(photo) {
-  const assets = await Asset.loadAsync(require('../../assets/yolov5s.ort'));
-  let modelPath = assets[0].localUri;
-  const session = await InferenceSession.create(modelPath);
+// async function Detect(photo) {
+//   const assets = await Asset.loadAsync(require('../../assets/yolov5s.ort'));
+//   let modelPath = assets[0].localUri;
+//   const session = await InferenceSession.create(modelPath);
 
-  const result = session.run(TensorConvert(photo), [
-    'num_detection:0',
-    'detection_classes:0',
-  ]);
-  console.log(result);
-}
+//   const result = session.run(TensorConvert(photo), [
+//     'num_detection:0',
+//     'detection_classes:0',
+//   ]);
+//   console.log(result);
+// }
 
-async function FetchAPI(source) {
+const FetchAPI = (source, [result, setResult]) => {
   let photo = {uri: source.uri};
   let formData = new FormData();
   formData.append('file', {
@@ -139,18 +139,23 @@ async function FetchAPI(source) {
       },
     })
     .then(response => {
-      console.log(response.data);
-      return response.data;
+      // console.log(response.data);
+      setResult(response.data);
+    })
+    .catch(err => {
+      console.log(err);
     });
-}
+};
 
 const ImageDetectPage = ({route, navigation}) => {
+  const [result, setResult] = useState(null);
   const photo = route.params;
   // Detect(photo);
-  const result = FetchAPI(photo);
+  FetchAPI(photo, [result, setResult]);
 
   return (
     <View style={styles.container}>
+      <Text>{result}</Text>
       <ImageBackground
         source={{uri: photo && photo.uri}}
         style={styles.container}
