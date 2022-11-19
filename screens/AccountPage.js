ed54c489f6a09057a95c487c844d8852ebe95a92
Phu <1911185@dlu.edu.vn>
2022-11-19 20:00:19 +0700

    Login

--- screens/AccountPage.js
+++ screens/AccountPage.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './LoginScreen';
+import HomeScreen from './HomeScreen';
+const Stack = createNativeStackNavigator();
 
-const AccountPage = () => {
+export default function App() {
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <NavigationContainer independent={true}>
+      <Stack.Navigator>
+        <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
+}
 
-export default AccountPage;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
