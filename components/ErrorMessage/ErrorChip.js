267624c01093f9fb51fe5f75b913ff4ed532128d
Thu Nguyen <thuthien2001@gmail.com>
2024-09-07 19:12:54 +0700

    Rename some style variables

--- components/ErrorMessage/ErrorChip.js
+++ components/ErrorMessage/ErrorChip.js
@@ -5,47 +5,58 @@ export const ErrorChip = ({status}) => {
   switch (status) {
     case 'success':
       return (
-        <View style={styles.errorBackgroundSuccess}>
-          <Text style={styles.errorMessage}>
+        <View style={styles.backgroundSuccess}>
+          <Text style={styles.message}>
             Đã tìm thấy sản phẩm! Vui lòng chọn kết quả bạn muốn sử dụng.
           </Text>
         </View>
       );
     case 'empty':
       return (
-        <View style={styles.errorBackgroundError}>
-          <Text style={styles.errorMessage}>
+        <View style={styles.backgroundError}>
+          <Text style={styles.message}>
             Không tìm thấy sản phẩm! Vui lòng thử lại.
           </Text>
         </View>
       );
     case 'failed':
       return (
-        <View style={styles.errorBackgroundError}>
-          <Text style={styles.errorMessage}>Không thể kết nối đến server.</Text>
+        <View style={styles.backgroundError}>
+          <Text style={styles.message}>Không thể kết nối đến server.</Text>
         </View>
       );
     default:
-      return null;
+      return (
+        <View style={styles.backgroundNeutral}>
+          <Text style={styles.message}>Working...</Text>
+        </View>
+      );
   }
 };
 
 const styles = StyleSheet.create({
-  errorBackgroundSuccess: {
+  backgroundSuccess: {
     backgroundColor: '#5FC314',
     marginHorizontal: 15,
     paddingVertical: 6,
     paddingHorizontal: 12,
     borderRadius: 10,
   },
-  errorBackgroundError: {
+  backgroundError: {
     backgroundColor: '#FF6901',
     marginHorizontal: 15,
     paddingVertical: 6,
     paddingHorizontal: 12,
     borderRadius: 10,
   },
-  errorMessage: {
+  backgroundNeutral: {
+    backgroundColor: '#4d4d4d',
+    marginHorizontal: 15,
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 10,
+  },
+  message: {
     color: 'white',
     textAlign: 'center',
   },
