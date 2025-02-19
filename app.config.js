ebca401a92faddda8eb44351b7911ee2a0f7f867
Thu Nguyen <thuthien2001@gmail.com>
2025-02-19 22:09:37 +0700

    Use static config along with dynamic config (better IDE support for static config)

--- app.config.js
+++ app.config.js
@@ -1,38 +1,25 @@
-module.exports = {
-  expo: {
-    name: 'SnapAndGo',
-    slug: 'snap-and-go',
-    version: '1.2.0',
-    orientation: 'portrait',
-    plugins: [
-      [
-        'expo-camera',
-        {
-          cameraPermission: 'Allow $(PRODUCT_NAME) to access your camera',
-          microphonePermission:
-            'Allow $(PRODUCT_NAME) to access your microphone',
+module.exports = ({config}) => {
+  return {
+    ...config,
+    expo: {
+      android: {
+        package: 'com.snapandgo',
+        permissions: [
+          'android.permission.CAMERA',
+          'android.permission.READ_EXTERNAL_STORAGE',
+          'android.permission.INTERNET',
+        ],
+        config: {
+          googleMaps: {
+            apiKey: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
+          },
         },
-      ],
-      'expo-build-properties',
-    ],
-    android: {
-      package: 'com.snapandgo',
-      permissions: [
-        'android.permission.CAMERA',
-        'android.permission.READ_EXTERNAL_STORAGE',
-        'android.permission.WRITE_EXTERNAL_STORAGE',
-        'android.permission.INTERNET',
-      ],
-      config: {
-        googleMaps: {
-          apiKey: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
+      },
+      ios: {
+        config: {
+          googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
         },
       },
     },
-    ios: {
-      config: {
-        googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_API_KEY,
-      },
-    },
-  },
+  };
 };
