f59dd684ba8b49f3a18c896cad3d680e70a5b889
Thu Nguyen <thuthien2001@gmail.com>
2025-02-24 20:12:32 +0700

    Timeout 5000

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
-  timeout: 1000,
+  timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
   },
