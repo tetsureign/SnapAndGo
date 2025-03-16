37da595405d15d54ee46916ad6926dba28e9ac73
Thu Nguyen <thuthien2001@gmail.com>
2025-03-16 22:08:58 +0700

    Separate the button!! yippeee

--- src/components/Buttons/index.ts
+++ src/components/Buttons/index.ts
@@ -2,3 +2,4 @@ export {default as GoButton} from './GoButton';
 export {default as ShowHideButton} from './ShowHideButton';
 export {default as TakePictureButton} from './TakePictureButton';
 export {default as ImagePickerButton} from './ImagePickerButton';
+export {default as GeneralButton} from './GeneralButton';
--- src/screens/Camera/components/DetectResultRenderer.styles.ts
+++ src/screens/Camera/components/DetectResultRenderer.styles.ts
@@ -3,27 +3,21 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   // Buttons
   detectedItemsButton: {
-    paddingVertical: 5,
-  },
-  selectedItemBackground: {
-    backgroundColor: '#646464',
-    borderWidth: 1,
-    borderColor: '#858585',
-    borderRadius: 10,
-    height: 40,
+    paddingBottom: 5,
   },
   itemBackground: {
+    padding: 5,
+    backgroundColor: 'transparent',
     borderWidth: 1,
     borderColor: 'transparent',
-    height: 40,
   },
   itemsTextContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
   itemsText: {
+    // TODO: Global style anyone??
     fontSize: 25,
-    paddingHorizontal: 5,
   },
   itemsTextWhite: {
     color: 'white',
