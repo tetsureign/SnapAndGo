3d0232f3be48e4b5b09805a45df0d8606698845f
Thu Nguyen <thuthien2001@gmail.com>
2025-03-17 20:38:47 +0700

    Various fix for actionsheet

--- src/components/ActionSheet/ActionSheet.tsx
+++ src/components/ActionSheet/ActionSheet.tsx
@@ -3,7 +3,11 @@ import ActionSheet, {
   ActionSheetRef,
   ActionSheetProps,
 } from 'react-native-actions-sheet';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+
 import {actionSheetStyle} from './ActionSheet.styles';
+import {DarkTheme} from '@/styles/theme';
 
 interface DarkPersistentActionSheetProps extends ActionSheetProps {
   innerRef: React.RefObject<ActionSheetRef>;
@@ -11,19 +15,27 @@ interface DarkPersistentActionSheetProps extends ActionSheetProps {
 
 // This one isn't meant to use with SheetManager.
 const DarkPersistentActionSheet = (props: DarkPersistentActionSheetProps) => {
+  const insets = useSafeAreaInsets();
+  const bottomTabHeight = useBottomTabBarHeight();
+
   return (
     <ActionSheet
       {...props}
       ref={props.innerRef}
+      children={props.children}
       backgroundInteractionEnabled={true}
-      containerStyle={actionSheetStyle.actionSheet}
+      containerStyle={{
+        backgroundColor: DarkTheme.colors.background,
+        borderRadius: DarkTheme.borderRadius.lg,
+        paddingBottom: bottomTabHeight,
+      }}
       headerAlwaysVisible={true}
       gestureEnabled={true}
       closable={false}
       drawUnderStatusBar={false}
-      indicatorStyle={actionSheetStyle.actionSheetIndicator}>
-      {props.children}
-    </ActionSheet>
+      safeAreaInsets={insets}
+      indicatorStyle={actionSheetStyle.actionSheetIndicator}
+    />
   );
 };
 
