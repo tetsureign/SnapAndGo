0e2b408a522cebd6777afdcc94a424a6814e6879
Thu Nguyen <thuthien2001@gmail.com>
2025-03-17 17:38:52 +0700

    Transparent Android navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {StatusBar} from 'react-native';
 import {SheetProvider} from 'react-native-actions-sheet';
+import * as NavigationBar from 'expo-navigation-bar';
 
 import TabsNavigator from './navigation/TabNavigator';
 import './components/ActionSheet/sheets.tsx';
 
 const App = () => {
+  useEffect(() => {
+    NavigationBar.setPositionAsync('absolute');
+    NavigationBar.setBackgroundColorAsync('transparent');
+  }, []);
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
