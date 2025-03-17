0d84774037a81e571147a9371dfc129da08df1a4
Thu Nguyen <thuthien2001@gmail.com>
2025-03-17 15:46:25 +0700

    Dynamic middle point for AC - But i will override this later...

---
+++ src/hooks/useActionSheetSnapPoint.ts
@@ -0,0 +1,30 @@
+import {useState, useMemo} from 'react';
+
+// Only used when ActionSheet height is dependent on the container (uses %)
+
+export const useActionSheetSnapPoint = (heightPercentage: number) => {
+  const [sheetContainerHeight, setSheetContainerHeight] = useState(1);
+  const [sheetChildrenHeight, setSheetChildrenHeight] = useState(1);
+
+  // const snapPoint =
+  //   (sheetChildrenHeight / (sheetContainerHeight * (heightPercentage / 100))) *
+  //   100;
+
+  const snapPoint = useMemo(() => {
+    return (
+      (sheetChildrenHeight /
+        (sheetContainerHeight * (heightPercentage / 100))) *
+      100
+    );
+  }, [sheetChildrenHeight, sheetContainerHeight, heightPercentage]);
+
+  console.log(snapPoint);
+
+  return {
+    sheetContainerHeight,
+    setSheetContainerHeight,
+    sheetChildrenHeight,
+    setSheetChildrenHeight,
+    snapPoint,
+  };
+};
